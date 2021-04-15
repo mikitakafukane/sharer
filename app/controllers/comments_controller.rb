b1cbb9a86ee9a8fa3a1ccfc143992933dfce4723
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-04-15 10:16:13 +0000

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    comment.save
-    redirect_to request.referer
-  end
-  
-  def destory
-    comment = Comment.find_by(post_id: params[:post_id], user_id: current_user.id)
-    comment.destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
     redirect_to posts_path
   end
-  
+
+  def destory
+    @post = Post.find(params[:post_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content)
--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module Sharer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  
+    
+    # config.logger = Logger.new(STDOUT)
+
 
   end
 end
