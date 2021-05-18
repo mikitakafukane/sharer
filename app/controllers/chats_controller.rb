df53db6844be9962340e6a56a6dc996d9ddf44c2
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-05-18 08:57:01 +0000

    チャット　非同期

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,13 +4,17 @@ class ChatsController < ApplicationController
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
-    redirect_to request.referer
+    @chats = Chat.where(room_id: @chat.room_id)
+                 .order(created_at: :desc)
+    # redirect_to request.referer
   end
 
   def destroy
     @chat = Chat.find(params[:id])
     @chat.destroy
-    redirect_to request.referer, notice: "チャット「#{@chat.content}」を削除しました"
+    # redirect_to request.referer, notice: "チャット「#{@chat.content}」を削除しました"
+    @chats = Chat.where(room_id: @chat.room_id)
+                 .order(created_at: :desc)
   end
 
   private
