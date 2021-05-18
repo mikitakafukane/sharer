90c2f72807d6405de2b2ab83bd1a66ed7d8b1766
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-05-18 08:58:22 +0000

    チャットコントローラー修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,13 +6,11 @@ class ChatsController < ApplicationController
     @chat.save
     @chats = Chat.where(room_id: @chat.room_id)
                  .order(created_at: :desc)
-    # redirect_to request.referer
   end
 
   def destroy
     @chat = Chat.find(params[:id])
     @chat.destroy
-    # redirect_to request.referer, notice: "チャット「#{@chat.content}」を削除しました"
     @chats = Chat.where(room_id: @chat.room_id)
                  .order(created_at: :desc)
   end
