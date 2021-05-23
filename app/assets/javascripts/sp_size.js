ba31d4e701e79160d48693c6e18857c8f2f674e3
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-05-23 17:18:13 +0000

    room index show　スマホ用画面に編集

--- app/assets/javascripts/sp_size.js
+++ app/assets/javascripts/sp_size.js
@@ -1,20 +1,18 @@
 /*global jQuery $*/
 jQuery(function ($) {
 
-if (window.matchMedia( "(max-width: 768px)" ).matches) {
-/* ウィンドウサイズが 768px以下の場合のコードをここに */
-	$(".accordion-content:not(:first-of-type)").css("display", "none");
-} else {
-/* ウィンドウサイズが 768px以上の場合のコードをここに */
-}
+  $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    // $(this).next().slideToggle(200);
+    // /*矢印の向きを変更*/
+    // $(this).toggleClass('open', 200);
+    $(".js-accordion-title").not(this).removeClass("open");
+    //クリックされたjs-accordion-title以外のcontentを閉じる
+    $(".js-accordion-title").not(this).next().slideUp(300);
+    //thisにopenクラスを付与
+    $(this).toggleClass("open");
+    //thisのcontentを展開、開いていれば閉じる
+    $(this).next().slideToggle(300);
+  });
 
-
-$('.js-accordion-title').on('click', function () {
-  /*クリックでコンテンツを開閉*/
-  $(this).next().slideToggle(200);
-  /*矢印の向きを変更*/
-  $(this).toggleClass('open', 200);
 });
-
-});
-
